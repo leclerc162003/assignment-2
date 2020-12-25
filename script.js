1cbcde71dbfd50334dcbef71378e47d30002ea34
leclerc162003 <S10203330@connect.np.edu.sg>
2020-12-25 22:54:23 +0800

    CORS error fixed

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
+// var link = 'https://cors-anywhere.herokuapp.com/';
+// var settings = {
+//     "url": "http://datamall2.mytransport.sg/ltaodataservice/BusArrivalv2?BusStopCode=28009" + link,
+//     "method": "GET",
+//     "timeout": 0,
+//     "headers": {
+//       "AccountKey": "qmXLs/m2RZG1kdj0DepVog=="
+//     },
+//   };
+  
+//   $.ajax(settings).done(function (response) {
+//     console.log(response);
+//   });
+
 var link = 'https://cors-anywhere.herokuapp.com/';
 var settings = {
-    "url": "http://datamall2.mytransport.sg/ltaodataservice/BusArrivalv2?BusStopCode=28009" + link,
+    "url": link + "http://datamall2.mytransport.sg/ltaodataservice/BusArrivalv2?BusStopCode=28009",
     "method": "GET",
     "timeout": 0,
     "headers": {
