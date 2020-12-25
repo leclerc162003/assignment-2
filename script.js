8d0f3fcbf017f4d370250d4ce8536b68943d8d0a
leclerc162003 <S10203330@connect.np.edu.sg>
2020-12-25 14:21:35 +0800

    attempt to fix CORS on JavaScript

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
+var link = 'https://cors-anywhere.herokuapp.com/';
 var settings = {
-    "url": "http://datamall2.mytransport.sg/ltaodataservice/BusArrivalv2?BusStopCode=28009",
+    "url": "http://datamall2.mytransport.sg/ltaodataservice/BusArrivalv2?BusStopCode=28009" + link,
     "method": "GET",
     "timeout": 0,
     "headers": {
