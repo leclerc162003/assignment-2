65ba04c88f4ae7af413b2c52a30fa72ed4abab52
leclerc162003 <S10203330@connect.np.edu.sg>
2020-12-31 17:15:21 +0800

    initial attempt to extract individual service information

--- script.js
+++ script.js
@@ -22,6 +22,27 @@ var settings = {
     },
   };
   
-  $.ajax(settings).done(function (response) {
+   $.ajax(settings).done(function (response) {
     console.log(response);
+    for( let i = 0; i < Services.length; i++){
+      if (Services[i].ServiceNo == "334"){
+        console.log(Services[i])
+      }
+    }
   });
+  
+  // function loadServices(){
+  //   var xhr = new XMLHttpRequest();
+  //   xhr.open("GET", link + "http://datamall2.mytransport.sg/ltaodataservice/BusArrivalv2?BusStopCode=28009", AccountKey = "qmXLs/m2RZG1kdj0DepVog==")
+
+  //   xhr.onload = function(){
+  //     if (this.status == 200){
+  //       var services = JSON.parse(this.responseText);
+  //       console.log(services);
+  //     }
+
+  //   }
+  //   xhr.send();
+  // }
+
+  // loadServices();
