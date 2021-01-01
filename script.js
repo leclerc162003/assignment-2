6df0b9ddb6011e6505c77ff59126af629d463aa4
leclerc162003 <S10203330@connect.np.edu.sg>
2021-01-01 23:38:51 +0800

    fixed extracting data from list of buses

--- script.js
+++ script.js
@@ -1,20 +1,6 @@
-// var link = 'https://cors-anywhere.herokuapp.com/';
-// var settings = {
-//     "url": "http://datamall2.mytransport.sg/ltaodataservice/BusArrivalv2?BusStopCode=28009" + link,
-//     "method": "GET",
-//     "timeout": 0,
-//     "headers": {
-//       "AccountKey": "qmXLs/m2RZG1kdj0DepVog=="
-//     },
-//   };
-  
-//   $.ajax(settings).done(function (response) {
-//     console.log(response);
-//   });
-
 var link = 'https://cors-anywhere.herokuapp.com/';
 var settings = {
-    "url": link + "http://datamall2.mytransport.sg/ltaodataservice/BusArrivalv2?BusStopCode=28009",
+    "url": link + "http://datamall2.mytransport.sg/ltaodataservice/BusArrivalv2?BusStopCode=28561",
     "method": "GET",
     "timeout": 0,
     "headers": {
@@ -22,27 +8,15 @@ var settings = {
     },
   };
   
-   $.ajax(settings).done(function (response) {
-    console.log(response);
-    for( let i = 0; i < Services.length; i++){
-      if (Services[i].ServiceNo == "334"){
-        console.log(Services[i])
-      }
+  $.ajax(settings).done(function (response) {
+    let result = response.Services;  
+    for (let i=0; i<result.length; i++) {
+        if (result[i].ServiceNo == "334") {
+          console.log(result[i])
+          document.getElementById('serviceno').textContent = result[i].ServiceNo;
+          document.getElementById('time').textContent = result[i].NextBus.EstimatedArrival;
+        }
     }
+
+    
   });
-  
-  // function loadServices(){
-  //   var xhr = new XMLHttpRequest();
-  //   xhr.open("GET", link + "http://datamall2.mytransport.sg/ltaodataservice/BusArrivalv2?BusStopCode=28009", AccountKey = "qmXLs/m2RZG1kdj0DepVog==")
-
-  //   xhr.onload = function(){
-  //     if (this.status == 200){
-  //       var services = JSON.parse(this.responseText);
-  //       console.log(services);
-  //     }
-
-  //   }
-  //   xhr.send();
-  // }
-
-  // loadServices();
