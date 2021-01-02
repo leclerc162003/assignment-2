7d715f3dc5e204932d14dcab6f461408e76a5da8
leclerc162003 <S10203330@connect.np.edu.sg>
2021-01-03 00:41:06 +0800

    improved outputs

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//ensuring cors error does not occur and keys 
 var link = 'https://cors-anywhere.herokuapp.com/';
 var settings = {
     "url": link + "http://datamall2.mytransport.sg/ltaodataservice/BusArrivalv2?BusStopCode=28561",
@@ -7,14 +8,58 @@ var settings = {
       "AccountKey": "qmXLs/m2RZG1kdj0DepVog=="
     },
   };
-  
+  //extracting data from API and printing
   $.ajax(settings).done(function (response) {
     let result = response.Services;  
     for (let i=0; i<result.length; i++) {
         if (result[i].ServiceNo == "334") {
           console.log(result[i])
           document.getElementById('serviceno').textContent = result[i].ServiceNo;
-          document.getElementById('time').textContent = result[i].NextBus.EstimatedArrival;
+          var time = (result[i].NextBus.EstimatedArrival).substring(11,19);
+          //document.getElementById('time').textContent = time;
+           var timeNow = new Date();
+           var hoursNow = timeNow.getHours();
+           var minutesNnow = timeNow.getMinutes();
+           var secondsNow = timeNow.getSeconds();
+           var current = hoursNow + ":" + minutesNnow + ":" + secondsNow;
+          
+          var time_start = new Date();
+          var time_end = new Date();
+          var value_start = current.split(':');
+          var value_end = time.split(':');
+
+          time_start.setHours(value_start[0], value_start[1], value_start[2], 0)
+          time_end.setHours(value_end[0], value_end[1], value_end[2], 0)
+
+          var diff = (time_end - time_start)/60000
+
+          if (diff <= 2){
+            
+            document.getElementById('time').textContent = "Arriving Soon...";
+            $(".display").css("border", "8px solid green");
+
+          }
+
+          else if (diff > 2 && diff <= 8){
+            
+            document.getElementById('time').textContent = Math.round(diff) + " " + "Minutes";
+            $(".display").css("border", "8px solid yellow");
+
+          }
+
+          else if (diff > 8){
+            
+            document.getElementById('time').textContent = Math.round(diff) + " " + "Minutes";
+            $(".display").css("border", "8px solid red");
+          }
+          else {
+            document.getElementById('time').textContent = "error";
+          }
+          
+
+          
+
+          
         }
     }
 
